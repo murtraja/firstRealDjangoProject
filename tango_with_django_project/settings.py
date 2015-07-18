74347556937c6317937a9c8dceb2a5b737191eb2
Murtaza <murtraja@gmail.com>
2015-07-19 00:21:21 +0530

    successfully used templates with static media just before 4.4

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -73,6 +73,9 @@ MEDIA_ROOT = ''
 # Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = ''
 
+STATIC_PATH = os.path.join(PROJECT_PATH, 'static')
+
+
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
@@ -88,6 +91,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    STATIC_PATH,
 )
 
 # List of finder classes that know how to find static files in
